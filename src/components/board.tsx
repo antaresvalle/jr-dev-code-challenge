2afec4b6222a73a7ac6e748ba15322accdd0c9ba
Antares Valle <antares.valle@gmail.com>
2025-09-03 15:52:51 -0700

    feat: show recent results

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement, useContext, useEffect, useState } from "react";
 import { Row } from "./row";
 import { GameContext } from "../contexts/game";
 import { Cell } from "../interfaces/IBoard";
+import { savedResults } from "@/utils/results";
+import { getResults } from "@/utils/results";
 
 interface AlertProps {
   tie: boolean;
@@ -22,6 +24,25 @@ function RenderResetButton(dispatch: (action: any) => void) {
   );
 }
 
+function RenderResults(array: { results: Array<string> }) {
+  if (!array.results || array.results.length === 0) return null;
+  return (
+    <div className="m-4">
+      <h3 className="font-bold mb-2">Results</h3>
+      {array.results.map((result, i) => {
+        return (
+          <p
+            className="bg-gray-200 border-b-gray-300 border-b-1 p-2"
+            key={result[i]}
+          >
+            {result}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
 function Alert({ tie, winner, symbol }: AlertProps) {
   const {
     dispatch,
@@ -33,6 +54,7 @@ function Alert({ tie, winner, symbol }: AlertProps) {
   const [playerOneName, setPlayerOneName] = useState("");
   const [playerTwoName, setPlayerTwoName] = useState("");
   const [computerName, setComputerName] = useState("");
+  const [results, setResults] = useState<string[]>([]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -45,31 +67,56 @@ function Alert({ tie, winner, symbol }: AlertProps) {
     }
   }, []);
 
+  useEffect(() => {
+    if (winner) {
+      let personalizedWinner = "";
+      if (gameType === "PVC") {
+        personalizedWinner = winner === symbol ? computerName : playerOneName;
+      } else {
+        personalizedWinner = winner === symbol ? playerOneName : playerTwoName;
+      }
+      setResults(savedResults(personalizedWinner) || []);
+    } else if (tie) {
+      setResults(savedResults("Tied game") || []);
+    }
+  }, [
+    winner,
+    tie,
+    gameType,
+    symbol,
+    playerOneName,
+    playerTwoName,
+    computerName,
+  ]);
+
   if (winner) {
     let personalizedWinner = "";
     if (gameType === "PVC") {
-      // TODO: name symbol in a more descriptive way
       // BUG: incorrect name showing sometimes
-      personalizedWinner = winner === symbol ? computerName : playerTwoName;
+      personalizedWinner = winner === symbol ? computerName : playerOneName;
     } else {
       personalizedWinner = winner === symbol ? playerOneName : playerTwoName;
     }
     return (
-      <div className="flex gap-x-4 justify-between bg-green-950 rounded-md p-4 mb-4">
-        <div className="text-white">
-          <strong className="font-normal py-1">Winner:</strong>{" "}
-          {personalizedWinner}
+      <div>
+        <div className="flex gap-x-4 justify-between bg-green-950 rounded-md p-4 mb-4">
+          <div className="text-white">
+            <strong className="font-normal py-1">Winner:</strong>{" "}
+            {personalizedWinner}
+          </div>
+          {RenderResetButton(dispatch)}
         </div>
-        {RenderResetButton(dispatch)}
       </div>
     );
   }
 
   if (tie) {
     return (
-      <div className="flex gap-x-4 justify-between bg-green-950 rounded-md p-4 mb-4">
-        <strong className="font-normal text-white">Tied game</strong>
-        {RenderResetButton(dispatch)}
+      <div>
+        <div className="flex gap-x-4 justify-between bg-green-950 rounded-md p-4 mb-4">
+          <strong className="font-normal text-white">Tied game</strong>
+          {RenderResetButton(dispatch)}
+        </div>
       </div>
     );
   }
@@ -96,6 +143,10 @@ function GameTypeToggle() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
+      const saved = localStorage.getItem("preferredGameMode");
+      if (saved && saved !== gameType) {
+        localStorage.removeItem("results");
+      }
       localStorage.setItem("preferredGameMode", gameType);
     }
   }, [gameType]);
@@ -126,10 +177,20 @@ export function Board() {
   } = useContext(GameContext);
 
   const [startingPlayerSymbol, setStartingPlayerSymbol] = useState("");
+  const [scoreBoard, setScoreBoard] = useState<{
+    results: Array<string>;
+  } | null>(null);
 
   useEffect(() => {
     setStartingPlayerSymbol(player);
+    const results = getResults();
+    setScoreBoard(results);
   }, []);
+
+  useEffect(() => {
+    const results = getResults();
+    setScoreBoard(results);
+  }, [winner, tie]);
 
   const rows: ReactElement[] = [];
 
@@ -142,6 +203,8 @@ export function Board() {
       <Alert winner={winner} tie={tie} symbol={startingPlayerSymbol} />
 
       <GameTypeToggle />
+
+      <div>{scoreBoard && RenderResults(scoreBoard)}</div>
 
       <div className="grid grid-cols-3 grid-rows-3 text-center max-h-[calc(100vh-10rem)] min-h-[calc(100vh-10rem)]">
         {rows.map((r) => r)}
