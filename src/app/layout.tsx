ad6e4df5bd1056cdcb6b7f5fd52418edb811ce6e
Antares Valle <antares.valle@gmail.com>
2025-09-03 12:22:41 -0700

    feat: add navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +27,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className="flex justify-center gap-6">
+          <Link href="/" className="text-blue-950 hover:text-blue-900">
+            Game
+          </Link>
+          <Link href="/settings" className="text-blue-950 hover:text-blue-900">
+            Settings
+          </Link>
+        </nav>
         {children}
       </body>
     </html>
